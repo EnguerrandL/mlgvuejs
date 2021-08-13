e2793d2a043a3e218f70e1b02610a54c144cfec6
EnguerrandL <louis.enguerrand84@gmail.com>
2021-08-13 22:31:26 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,40 @@
       color="orange lighten-1"
       dark
     >
-      <div class="d-flex align-center">
+      <!-- <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
+          id="breathing"
           contain
           src="https://www.netmedia.fr/wp-content/uploads/2020/10/Logos-NETMEDIA-2020-BLANC.png"
           transition="scale-transition"
           width="200"
         />
-      </div>
+      </div> -->
       <v-spacer></v-spacer>
         <div class="h1 mr-6" >Mentions Légales Générateur</div>
     </v-app-bar>
 
     <v-main >
-        <v-container>
-              <v-layout>
+        <v-container id="" bg fill-height  align="center" justify="center" >
+              <v-layout class="mainBack">
+          
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          id="breathing"
+          
+          contain
+          src="https://www.netmedia.fr/wp-content/uploads/2020/10/Logos-NETMEDIA-2020-BLANC.png"
+          width="500"
+        />
+         
 
 
                     
-      <MlgForm  />
+      <!-- <MlgForm  /> -->
+      <!-- <ComingSoon  /> -->
 
 
 
@@ -58,7 +71,8 @@
 </template>
 
 <script>
-import MlgForm from './components/MlgForm';
+// import MlgForm from './components/MlgForm';
+// import ComingSoon from './components/ComingSoon';
 
 
 
@@ -69,7 +83,8 @@ export default {
   name: 'App',
 
 components: {
-    MlgForm,
+    // MlgForm,
+    // ComingSoon,
 
   },
       method: {
@@ -92,3 +107,72 @@ components: {
 
 };
 </script>
+
+<style lang="css">
+
+.mainBack{
+    background-color: #FFA726;
+
+
+
+}
+
+#breathing {
+    margin-left: auto !important;
+    margin-right: auto !important;
+    -webkit-animation: breathing 5s ease-out infinite normal;
+    animation: breathing 5s ease-out infinite normal;
+ 
+    }
+
+
+@-webkit-keyframes breathing {
+  0% {
+    -webkit-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+
+  25% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+
+  60% {
+    -webkit-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+
+  100% {
+    -webkit-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+}
+
+@keyframes breathing {
+  0% {
+    -webkit-transform: scale(0.9);
+    -ms-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+
+  25% {
+    -webkit-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+  }
+
+  60% {
+    -webkit-transform: scale(0.9);
+    -ms-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+
+  100% {
+    -webkit-transform: scale(0.9);
+    -ms-transform: scale(0.9);
+    transform: scale(0.9);
+  }
+}
+
+  
+</style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,66 @@
+<template> 
+
+      <v-col
+        
+          class="mainBack"
+        >
+  <v-row align="center"
+      justify="center">
+      <v-col>
+              <div class="d-flex  justify-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          id="breathing"
+          contain
+          src="https://www.netmedia.fr/wp-content/uploads/2020/10/Logos-NETMEDIA-2020-BLANC.png"
+          width="500"
+        />
+         
+      </div>
+      </v-col>
+  </v-row>
+         
+  
+
+           </v-col>
+
+
+
+</template>
+<script>
+   
+
+  export default {
+
+    name: 'ComingSoon',
+
+    data() {
+      return {
+ 
+
+        
+      }
+
+      
+    },
+    
+
+        methods: {
+
+        }
+    
+  }
+
+
+  
+</script>
+
+<style lang="css">
+.mainBack{
+    background-color: #FFA726;
+
+
+}
+    
+</style>
